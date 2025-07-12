397cc33d24ae9116d0378baa0487613722558dbc
João Marcelo <79545726+jmcanario1@users.noreply.github.com>
2025-07-12 10:35:03 -0300

    Refactor ETL pipeline to encapsulate logic within the main function

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -1,9 +1,15 @@
 from etl import extract, transform, load
 
-extracted_data = extract('data/raw/superstore.csv')
+def main():
+  extracted_data = extract('data/raw/superstore.csv')
 
-if extracted_data is not None:
-  transformed_data = transform(extracted_data)
+  if extracted_data is not None:
+    transformed_data = transform(extracted_data)
 
-if transformed_data is not None:
-  load(transformed_data)
+  if transformed_data is not None:
+    load(transformed_data)
+
+
+
+if __name__ == "__main__":
+  main()
